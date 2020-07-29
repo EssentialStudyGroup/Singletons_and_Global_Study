4e2651a981bb545f36835fb93264b6c52c074c70
Tsungyu Yu <ytyubox@gmail.com>
2020-07-29 15:26:35 +0800

    using extension to module

--- Tests/Singletons_and_Global_StudyTests/demo.swift
+++ Tests/Singletons_and_Global_StudyTests/demo.swift
@@ -4,11 +4,43 @@ class DemoTests:XCTestCase {
         
     }
 }
-
+// MARK: - copy from video
+class MockApiClient: ApiClient { }
+// MARK: - Api Module
 class ApiClient {
     private init() {}
     
     internal static
         func make_singleton() -> ApiClient {ApiClient()}
     static let Singleton = ApiClient()
+    func execute(_ request: URLRequest,
+                 completion:(Date)->Void) {}
+}
+// MARK: - Login Module
+struct LoggedInUser {}
+extension ApiClient {
+    func login(completion:(LoggedInUser) -> Void) {}
+}
+class LoginViewController {
+    var api = ApiClient.Singleton
+    func didTapLoginButton() {
+        api.login {
+            user in
+            // show next screen
+        }
+    }
+}
+// MARK: - Feed Module
+struct FeedItem {}
+extension ApiClient {
+    func loadFeed(completion:([FeedItem]) -> Void) {}
+}
+class FeedViewController {
+    var api = ApiClient.Singleton
+    func viewDidLoad() {
+        api.loadFeed() {
+            items in
+            
+        }
+    }
 }
